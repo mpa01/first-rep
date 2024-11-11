f67bfe9af6790836f39ac3fefaca6a7c4a023a51
mpa01 <mpa01@iesemilidarder.com>
2024-11-11 19:51:32 +0100

    Taweno

---
+++ PRG/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    
+    }
+    
+}
